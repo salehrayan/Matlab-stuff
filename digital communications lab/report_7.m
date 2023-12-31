f20d4c1b63d29d2a94e43e1d6a21e0b323ba6d00
salehrayan <138001028+salehrayan@users.noreply.github.com>
2023-12-31 14:53:21 +0330

    s

--- digital communications lab/report_7.m
+++ digital communications lab/report_7.m
@@ -3,12 +3,23 @@ syms q
 
 gamma_s = -4:0.5:14;
 gamma_lin = 10.^(gamma_s./10);
-% gamma_lin = 1;
-M = 2;
+markers = 'sxvpo';
 
-for i=1:length(gamma_lin)
-    f(q) = (1./2.*erfc(-q - sqrt(gamma_lin(i).*2))).^(M-1)./(sqrt(2.*pi)).*exp(-q.^2./2);
-    SER(i) = 1- int(f, q, -inf, inf)
+for n = 1:5
+    M = 2.^n;
+    x = log2(M).*gamma_lin;
+    for i=1:length(gamma_lin)
+        f(q) = (1./2.*erfc(-q - sqrt(x(i).*2))).^(M-1)./(sqrt(2.*pi)).*exp(-q.^2./2);
+        SER(i) = 1- int(f, q, -inf, inf);
+    end
+    semilogy(gamma_s, SER, 'DisplayName', [num2str(M) '-FSK'], LineWidth=1.5, Marker=markers(n)) 
+    hold on
 end
+
+legend('Location','southwest')
+ylim([10.^(-15) 1])
+ylabel('SER')
+xlabel('SNR (dB)')
+
 
 
